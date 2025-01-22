2149201892f0a08288ea65ddc44bbc428140af2c
fareshany <fareeeshany@gmail.com>
2025-01-22 12:59:48 -0800

    Add project files.

---
+++ C#ADVANCED/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C_ADVANCED
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
